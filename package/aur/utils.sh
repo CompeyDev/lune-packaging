a147bd1592845103e4aa73a3419cba38f143d050
Erica Marigold <hi@devcomp.xyz>
2023-07-23 21:32:11 +0530

    feat(packaging_aur): create utils script

---
+++ package/aur/utils.sh
@@ -0,0 +1,17 @@
+function increment_version() {
+ local v=$1
+ if [ -z $2 ]; then 
+    local rgx='^((?:[0-9]+\.)*)([0-9]+)($)'
+ else 
+    local rgx='^((?:[0-9]+\.){'$(($2-1))'})([0-9]+)(\.|$)'
+    for (( p=`grep -o "\."<<<".$v"|wc -l`; p<$2; p++)); do 
+       v+=.0; done; fi
+ val=`echo -e "$v" | perl -pe 's/^.*'$rgx'.*$/$2/'`
+ echo "$v" | perl -pe s/$rgx.*$'/${1}'`printf %0${#val}s $(($val+1))`/
+}
+
+function get_current_version() {
+  current_version=`cat PKGBUILD.git | grep pkgver | head -n 1 | cut -d '=' -f 2`
+
+  printf current_version
+}
