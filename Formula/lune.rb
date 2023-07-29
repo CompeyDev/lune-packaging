790b18a1ad1a1c0839f54d93027dab1c7b108bf5
Jack Programs <52725440+jackprogramsjp@users.noreply.github.com>
2023-07-29 10:58:27 -0700

    Cargo.toml was moved to root in Lune, so changed the arguments of cargo install

--- Formula/lune.rb
+++ Formula/lune.rb
@@ -8,7 +8,7 @@ class Lune < Formula
   depends_on "rust" => :build
 
   def install
-    system "cargo", "install", "--locked", "--root", prefix, "--path", "./packages/cli"
+    system "cargo", "install", *std_cargo_args
   end
 
   test do
