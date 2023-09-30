891312c0a6dd6b1004c19fb3fda65d0edec3c4c5
Erica Marigold <hi@devcomp.xyz>
2023-09-30 13:20:03 +0530

    feat: include logging

--- package/action/src/download.rs
+++ package/action/src/download.rs
@@ -17,11 +17,20 @@ use crate::{
 };
 
 fn parse_release_data(data: &LuneReleaseData) -> LuneReleaseFetched {
+    const TARGET: &str = "download::parse_release_data";
+
+    tracing::info!(target: TARGET, "Parsing received LuneReleaseData");
+
     let unknown = &String::from("UNKNOWN");
 
     let mut version = unknown.to_owned();
     let mut download = unknown.to_owned();
     let mut artifact_name = unknown.to_owned();
+
+    tracing::info!(
+        target: TARGET,
+        "Defaulting to `unknown` values before parsing"
+    );
 
     for asset in &data.assets {
         let parts = asset
@@ -36,11 +45,24 @@ fn parse_release_data(data: &LuneReleaseData) -> LuneReleaseFetched {
         // [3] => arch .. ".zip"
 
         if parts[2] == env::consts::OS && parts[3].trim_end_matches(".zip") == env::consts::ARCH {
+            tracing::info!(
+                target: TARGET,
+                "Supported platform found, overwriting `unknown` values"
+            );
+
             version = (&parts[1]).to_owned();
             download = (&asset.browser_download_url).to_owned();
             artifact_name = (&asset.name).to_owned();
+        } else {
+            tracing::warn!(
+                target: TARGET,
+                "Supported platform not found for asset {:?}",
+                asset
+            );
         }
     }
+
+    tracing::info!(target: TARGET, "Done.");
 
     LuneReleaseFetched {
         version,
@@ -51,10 +73,24 @@ fn parse_release_data(data: &LuneReleaseData) -> LuneReleaseFetched {
 }
 
 pub fn download_release() -> Result<(PathBuf, LuneReleaseFetched)> {
+    const TARGET: &str = "download::download_release";
+
+    tracing::info!(target: TARGET, "Initializing routine");
+
     let parsed_release_data =
         parse_release_data(&GitHub::new(("filiptibell", "lune")).fetch_releases()?);
 
+    tracing::info!(target: TARGET, "Received API resp and parsed release data");
+    tracing::info!(target: TARGET, "Got lune {}", parsed_release_data.version);
+
     let fetcher = Fetcher::new();
+
+    tracing::info!(
+        target: TARGET,
+        "Proceeding to download release from API resp (url = {})",
+        &parsed_release_data.download
+    );
+
     let resp = fetcher.fetch::<_, ()>(
         Method::Get,
         Url::from_str(&parsed_release_data.download.as_str())?,
@@ -63,6 +99,12 @@ pub fn download_release() -> Result<(PathBuf, LuneReleaseFetched)> {
 
     match resp {
         FetchResult::Response(res) => {
+            tracing::info!(
+                target: TARGET,
+                "Received download resp, proceeding to write zip to disk at {}",
+                &parsed_release_data.artifact_name
+            );
+
             let mut zip_archive = File::create(&parsed_release_data.artifact_name)?;
 
             let mut bytes = res
@@ -71,7 +113,9 @@ pub fn download_release() -> Result<(PathBuf, LuneReleaseFetched)> {
                 .map(|elem| elem.unwrap())
                 .collect::<Vec<u8>>();
 
-            zip_archive.write_all(&mut bytes)?
+            zip_archive.write_all(&mut bytes)?;
+
+            tracing::info!(target: TARGET, "Successfully downloaded release");
         }
         FetchResult::Result(_) => unreachable!("Fetcher returned Result instead of Response"),
     };
@@ -83,10 +127,18 @@ pub fn download_release() -> Result<(PathBuf, LuneReleaseFetched)> {
 }
 
 pub fn install_lune(lune_archive_reader: impl Read + Seek, meta: LuneReleaseFetched) -> Result<()> {
+    const TARGET: &str = "download::install_lune";
+
+    tracing::info!(target: TARGET, "Initializing routine");
+
     let dir_name = meta.artifact_name.trim_end_matches(".zip");
+
+    tracing::info!(target: TARGET, "Proceeding to extract release zip");
 
     let mut lune_zip = ZipArchive::new(lune_archive_reader)?;
     lune_zip.extract(dir_name)?;
+
+    tracing::info!(target: TARGET, "Successfully extracted release zip");
 
     let bin_name = match env::consts::OS {
         "windows" => "lune.exe",
@@ -103,14 +155,26 @@ pub fn install_lune(lune_archive_reader: impl Read + Seek, meta: LuneReleaseFetc
         .join("bin");
 
     if !bin_base_dir.exists() {
+        tracing::warn!(target: TARGET, "bin_base_dir nonexistent, creating");
         std::fs::create_dir(&bin_base_dir)?;
     }
+
+    tracing::warn!(
+        target: TARGET,
+        "macOS and windows support for this action is incomplete"
+    );
 
     // TODO: macOS and windows support
     let lune_bin_path = bin_base_dir.join(bin_name);
 
     File::create(&lune_bin_path)?;
-    std::fs::rename(PathBuf::from(dir_name).join(bin_name), lune_bin_path)?;
+    std::fs::rename(PathBuf::from(dir_name).join(bin_name), &lune_bin_path)?;
+
+    tracing::info!(
+        target: TARGET,
+        "Installed lune bin to {}",
+        lune_bin_path.to_string_lossy()
+    );
 
     Ok(())
 }
--- package/action/src/main.rs
+++ package/action/src/main.rs
@@ -1,11 +1,21 @@
 use std::fs::File;
 
 use action::download::{download_release, install_lune};
+use tracing::Level;
+use tracing_unwrap::ResultExt;
 
 fn main() {
-    better_panic::install();
+    if cfg!(debug_assertions) {
+        better_panic::install();
+    } else {
+        // Check for is_debug in github actions
+        tracing_subscriber::fmt()
+            .with_max_level(Level::DEBUG)
+            .init();
+    }
 
-    let (zip_path, meta) = download_release().expect("failed to download latest lune release");
+    let (zip_path, meta) =
+        download_release().expect_or_log("failed to download latest lune release");
 
     install_lune(
         File::open(&zip_path).expect(
@@ -17,5 +27,7 @@ fn main() {
         ),
         meta,
     )
-    .expect("failed to install lune. did we not have perms to write to the required directories?");
+    .expect_or_log(
+        "failed to install lune. did we not have perms to write to the required directories?",
+    );
 }
