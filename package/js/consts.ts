a5ed0c9fb87d821c054c4d63d1f78c9374c5c54f
Erica Marigold <hi@devcomp.xyz>
2024-06-21 12:36:45 +0530

    feat(consts): move constants from toml into module

--- package/js/consts.ts
+++ package/js/consts.ts
@@ -1,12 +1,5 @@
-import { parse as parseToml } from "jsr:@std/toml";
-import * as path from "jsr:@std/path";
-
-export const BASE_PATH = path.dirname(path.fromFileUrl(import.meta.url));
-
-export default parseToml(
-	Deno.readTextFileSync(path.join(BASE_PATH, "consts.toml")),
-) as {
-	name: string;
-	api_url: string;
-	version: string;
-};
+export default {
+	name: "lune",
+	version: "0.8.5",
+	api_url:  "https://api.github.com/repos/lune-org/lune/releases"
+} as const;
