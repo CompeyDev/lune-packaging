fd91ba6190d58f210f5df809ac3abe229ee2f1ee
Erica Marigold <hi@devcomp.xyz>
2024-06-21 13:12:16 +0530

    fix(deno): use $HOME/.lune/versions as base path

--- package/js/consts.ts
+++ package/js/consts.ts
@@ -1,5 +1,12 @@
+import * as path from "jsr:@std/path";
+import { ensureDir } from "jsr:@std/fs";
+import home_dir from "https://deno.land/x/dir@1.5.2/home_dir/mod.ts";
+
+export const BASE_PATH = path.join(home_dir()!, ".lune", "versions");
+await ensureDir(BASE_PATH);
+
 export default {
 	name: "lune",
 	version: "0.8.5",
-	api_url:  "https://api.github.com/repos/lune-org/lune/releases"
+	api_url: "https://api.github.com/repos/lune-org/lune/releases",
 } as const;
