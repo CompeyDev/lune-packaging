485be013c40cff518ab0be83ba25296a9f598192
Erica Marigold <hi@devcomp.xyz>
2025-08-03 14:05:48 +0100

    feat(js): lune v0.10.0

--- package/js/consts.ts
+++ package/js/consts.ts
@@ -7,6 +7,6 @@ await ensureDir(BASE_PATH);
 
 export default {
 	name: "lune",
-	version: "0.9.4",
+	version: "0.10.0",
 	api_url: "https://api.github.com/repos/lune-org/lune/releases",
 } as const;
